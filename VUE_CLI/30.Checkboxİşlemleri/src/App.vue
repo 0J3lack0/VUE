1a019980d7bccb9fe58b1c2ec881df1cbee6200d
0J3lack0 <g.caglar.c@gmail.com>
2024-11-14 01:35:46 +0300

    yenş öğrenilen konular

---
+++ VUE_CLI/30.Checkboxİşlemleri/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <add-blog> </add-blog>
+</template>
+
+<script>
+import AddBlog from "./components/add-blog.vue";
+export default {
+  components: {
+    "add-blog": AddBlog,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
